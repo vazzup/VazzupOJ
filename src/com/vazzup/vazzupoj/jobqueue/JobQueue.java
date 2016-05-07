c962a0e5149068646a7625ea083a5f0080f0d949
Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>
2016-05-07 22:34:40 +0530

    Scripts updated; Threads for each language created; Comparison program added;

--- src/com/vazzup/vazzupoj/jobqueue/JobQueue.java
+++
@@ -1,9 +0,0 @@
-package com.vazzup.vazzupoj.jobqueue;
-
-import com.vazzup.vazzupoj.job.*;
-import java.util.*;
-
-public class JobQueue extends PriorityQueue<Job>{
-	private static final long serialVersionUID = 1L;
-	
-}
